4dfc5443122e17b035ccd76680933edc01d76a02
Eskimos <xweird.sun@gmail.com>
2018-06-20 14:19:16 +0300

    pnp model added

---
+++ models/PNP/HMI/Canvas.cnv.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by nxtSTUDIO.
+ * User: XPMUser
+ * Date: 2/8/2012
+ * Time: 7:36 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using System.Drawing;
+
+using NxtControl.GuiFramework;
+
+
+namespace NxtStudio.Canvases
+{
+	/// <summary>
+	/// Description of Canvas.
+	/// </summary>
+	public partial class Canvas : NxtControl.GuiFramework.HMICanvas
+	{
+		public Canvas()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ models/PNP/HMI/PnPView/PnPView_Faceplate.cnv.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by nxtSTUDIO.
+ * User: XPMUser
+ * Date: 2/8/2012
+ * Time: 7:03 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using System.Drawing;
+using NxtControl.GuiFramework;
+using System.Windows.Forms;
+
+namespace NxtStudio.Faceplates.PnPView
+{
+	/// <summary>
+	/// Description of Faceplate.
+	/// </summary>
+	public partial class Faceplate : NxtControl.GuiFramework.HMIFaceplate
+	{
+	  bool wp1Selected;
+	  bool wp2Selected;
+	  bool wp3Selected;
+
+		public Faceplate()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void AddWPButtonClick(object sender, EventArgs e)
+		{
+		  wp1Selected = wp1.Checked;
+			wp2Selected = wp2.Checked;
+			wp3Selected = wp3.Checked;
+			FireEvent_CNF(wp1Selected, wp2Selected, wp3Selected);
+			//MessageBox.Show("Yea", "Yayy");
+		}
+	}
+}
